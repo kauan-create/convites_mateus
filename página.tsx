d466c7daef35a343ae444f1dca70bdd1c6a3ccfa
Kauan Rodrigues <kauanelison@gmail.com>
2026-06-04 20:58:17 -0300

    fix: remove pagina com acento e adiciona xlsx

--- página.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Page() {
-
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Festa do Mateus! 🎉</h1>
-      <p>Site em construção...</p>
-    </main>
-  )
-}
